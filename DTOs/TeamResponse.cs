87641557a49067f39c810fd14ca6b829521406d6
Edson Candido <edson.candido@chartindustries.com>
2025-04-09 21:08:47 -0300

    pesquisa de time funcionando e adicionado biblioteca fontawesome

---
+++ DTOs/TeamResponse.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CartolaLigas.DTOs
+{
+    public class TeamDTO
+    {
+        [JsonPropertyName("url_escudo_svg")]
+        public string UrlEscudoSvg { get; set; }
+
+        [JsonPropertyName("nome_cartola")]
+        public string NomeCartola { get; set; }
+
+        [JsonPropertyName("slug")]
+        public string Slug { get; set; }
+
+        [JsonPropertyName("url_escudo_png")]
+        public string UrlEscudoPng { get; set; }
+
+        [JsonPropertyName("nome")]
+        public string Nome { get; set; }
+
+        [JsonPropertyName("foto_perfil")]
+        public string FotoPerfil { get; set; }
+
+        [JsonPropertyName("descricao")]
+        public string Descricao { get; set; }
+
+        [JsonPropertyName("facebook_id")]
+        public long? FacebookId { get; set; }
+
+        [JsonPropertyName("time_id")]
+        public long TimeId { get; set; }
+
+        [JsonPropertyName("assinante")]
+        public bool? Assinante { get; set; }
+    }
+    public class TeamResponse
+    {
+    }
+}
